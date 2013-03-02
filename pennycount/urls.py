328f34406ca8bb62eba1b1bf78c1bc2c723de849
Felix Kerekes <sttwister@gmail.com>
2013-03-02 14:07:25 +0200

    Django skeleton is good

--- pennycount/urls.py
+++ pennycount/urls.py
@@ -1,17 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pennycount.views.home', name='home'),
-    # url(r'^pennycount/', include('pennycount.foo.urls')),
+urlpatterns = patterns('pennycount.views',
+    url(r'^$', 'index'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
