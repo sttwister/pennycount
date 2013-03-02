16a291908cc7d694591c068fa055fe6fea3047ec
Felix Kerekes <sttwister@gmail.com>
2013-03-02 17:56:53 +0200

    Added friends to fixtures

--- pennycount/admin.py
+++ pennycount/admin.py
@@ -7,5 +7,9 @@ class PaymentAdmin(admin.ModelAdmin):
     """Custom admin for Payment."""
     list_display = ('user', 'title', 'value')
 
+class FriendAdmin(admin.ModelAdmin):
+    """Custom admin for Friend."""
+    list_display = ('user', 'friend')
+
 admin.site.register(Payment, PaymentAdmin)
-admin.site.register(Friend)
+admin.site.register(Friend, FriendAdmin)
